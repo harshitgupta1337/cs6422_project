2b872dd5f8721cd418273ac38b3519293ac941fc
Harshit Gupta <harshitgupta1337@gmail.com>
2019-03-21 20:30:17 -0400

    Fixing window miscalculation

--- system_simulator.py
+++ system_simulator.py
@@ -9,12 +9,14 @@ server_latency_dists = []
 server_last_update_ts = {}
 server_last_latency_sample = {}
 
+trans_attempts = {}
+
 def generate_transaction_arrivals(num_transactions):
     arrivals = []
     t = 10
     for i in range(num_transactions):
         arrivals.append(t)
-        t += 100
+        t += 40
     return arrivals
 
 def run_system():
@@ -30,15 +32,19 @@ def get_prob_changed(server_idx, trans_start_ts, trans_idx, latencies, auto_rate
     if server_idx not in server_last_update_ts.keys():
         return 0.0
     else:
-        window = server_last_update_ts[server_idx] + server_last_latency_sample[server_idx] + latencies[0]
-
+        window = trans_start_ts + latencies[0] - server_last_update_ts[server_idx]
         exponent = -1 * auto_rate * window
         return (1 - math.exp(exponent))
 
 # This function should return the transaction completion time
 def process_transaction(trans_idx, trans_start_ts):
+    global trans_attempts
     trans_finish_time = trans_start_ts
     while (True):
+        if trans_idx not in trans_attempts.keys():
+            trans_attempts[trans_idx] = 0
+        trans_attempts[trans_idx] += 1
+
         server_idxs = random.sample(range(N), W)
         
         prob_success = 1
@@ -67,7 +73,7 @@ def process_transaction(trans_idx, trans_start_ts):
                 trans_finish_time = ack_recv_time
  
         r = random.uniform(0,1)
-   
+  
         if r >= prob_success:
             #  transaction has failed
             trans_start_ts = trans_finish_time
@@ -79,7 +85,7 @@ def process_transaction(trans_idx, trans_start_ts):
 
 def main():
     global N, W, auto_rate, lat_mean, lat_stddev
-    N = 10
+    N = 100
     W = 4
     auto_rate = 0.00001 # this is per millisecond
     lat_mean = 10
@@ -106,7 +112,7 @@ def main():
 
         finish_time = process_transaction(trans_idx, curr_time)
         trans_execn_latency = finish_time - transaction_arrivals[trans_idx]
-        print ("Trans execn latency  for trans ", trans_idx, " = ", trans_execn_latency)
+        print ("TransID\t", trans_idx, "\tstart_delay\t", "%.2f"%(curr_time-transaction_arrivals[trans_idx]), "\tlatency\t", "%.2f"%trans_execn_latency, "\tnum_trials\t", trans_attempts[trans_idx])
         curr_time = finish_time
         last_finished_trans_idx = trans_idx
         trans_idx += 1
