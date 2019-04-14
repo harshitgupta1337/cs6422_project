6b381738efc4288bfe6840d940a6156c0c2d1e33
Harshit Gupta <harshitg@gatech.edu>
2019-04-14 16:51:43 -0400

    Adding threads to process requests

--- system/controller.py
+++ system/controller.py
@@ -6,6 +6,7 @@ import random
 import argparse
 import yaml
 
+from state.server_state import *
 from proto.commit_protocol_pb2 import *
 
 class RequestHandler(threading.Thread):
@@ -18,8 +19,13 @@ class RequestHandler(threading.Thread):
     def run(self):
         while True:
             req = self.request_queue.get()
-            print ("Was able to read an item")
             self.callback(req)
+
+class TransactionStruct:
+    def __init__(self, transaction_id, version_map):
+        self.dependencies = set()
+        self.version_map = version_map
+        self.id = transaction_id
 
 class Controller:
     def __init__(self, N, w, server_data):
@@ -27,12 +33,20 @@ class Controller:
         self.N = N
         self.w = w
         self.server_state = {}
+        self.transaction_id_counter = 0
         self.init_servers(server_data)
+        self.executing_transactions = {}
+        self.pending_transactions = {}
+        self.pending_trans_condition = threading.Condition()
 
         self.request_queue = queue.Queue()
         self.req_queue_condition = threading.Condition()
         self.request_handler = RequestHandler(self.request_queue, self.req_queue_condition, self.process_request)
         self.request_handler.start()
+        
+
+        self.trans_exec_thread = threading.Thread(target=self.transaction_executor)
+        self.trans_exec_thread.start()
 
     def __del__(self):
         self.request_handler.join()
@@ -41,9 +55,24 @@ class Controller:
         server_idxs = random.sample(range(self.N), self.w)
         return server_idxs
 
+    def increment_server_versions(self, server_idxs):
+        for server_idx in server_idxs:
+            self.server_state[server_idx].version.v1 += 1
+        print (self.server_state[server_idx].version.v1)
+
     def process_request(self, request):
         servers = self.select_servers(request)
-        
+        self.update_servers(servers)
+
+    def update_servers(self, server_idxs):
+        version_map = {}
+        for server_idx in server_idxs:
+            version = self.server_state[server_idx].version
+            version_map[server_idx] = version
+
+        transaction_id = self.transaction_id_counter
+        self.transaction_id_counter += 1
+        self.submit_transaction(transaction_id, version_map)
 
     def submit_request(self, request):
         self.request_queue.put(request)
@@ -54,9 +83,57 @@ class Controller:
         server_conf = yaml.load(yaml_stream)
         idx = 0
         for server in server_conf:
-            self.server_state[idx] = server
+            cpu = server["cpu"]
+            memory = server["memory"]
+            self.server_state[idx] = ServerState(idx, cpu, memory)
             idx += 1
-        
+
+    # Transaction code
+    def submit_transaction(self, transaction_id, version_map):
+        t = TransactionStruct(transaction_id, version_map)
+        map_t = version_map.keys()
+
+        for e in self.executing_transactions.keys():
+            map_e = self.executing_transactions[e].version_map.keys()
+            if bool(set(map_t) & set(map_e)):
+                # there is an overlap
+                t.dependencies.add(self.executing_transactions[e].id)
+
+        for p in self.pending_transactions.keys():
+            map_p = self.pending_transactions[p].version_map.keys()
+            if bool(set(map_t) & set(map_p)):
+                # there is an overlap
+                t.dependencies.add(self.pending_transactions[p].id)
+
+        print ("Adding transaction id %d to pending transactions"%transaction_id)
+        self.pending_trans_condition.acquire()
+        self.pending_transactions[transaction_id] = t
+        self.pending_trans_condition.notify()
+        self.pending_trans_condition.release()
+
+    def process_transaction_failure(self, transaction_id, server_replies):
+        pass
+
+    def process_transaction_success(self, transaction_id, server_replies):
+        pass
+
+    def transaction_executor(self):
+        while True:
+            self.pending_trans_condition.acquire()
+            to_execute = []
+            for pend_trans_id in self.pending_transactions.keys():
+                if len(self.pending_transactions[pend_trans_id].dependencies) == 0:
+                    to_execute.append(pend_trans_id)
+            
+            if len(to_execute) == 0:
+                self.pending_trans_condition.wait()
+            else:
+                # process the transactions in to_execute
+                self.executing_transactions[pend_trans_id] = self.pending_transactions[pend_trans_id]
+                self.pending_transactions.pop(pend_trans_id, None)
+                print ("Executing %s"%pend_trans_id)
+            self.pending_trans_condition.release()        
+
 def merge_args(yaml_conf, cmdline_args):
     if cmdline_args.w:
         yaml_conf['w'] = cmdline_args.w
@@ -85,4 +162,3 @@ if __name__ == "__main__":
     while True:
         controller.submit_request(None)
         time.sleep (1)
-        print ("Iteration of main thread")
---
+++ system/state/server_state.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.insert(0, "/home/harshitg/Courses/CS6422/cs6422_project/system")
+
+from proto.commit_protocol_pb2 import *
+
+'''
+    This class stores the state of a server
+'''
+class ServerState:
+    
+    def __init__(self, server_id, max_cpu, max_memory):
+        self.server_id = server_id
+        self.allocations = {}
+        self.max_cpu = max_cpu
+        self.max_memory = max_memory
+        self.version = Version()
+        self.version.v1 = 0
+        self.version.v2 = 0
+
+    def update_allocation(self, task_id, cpu, memory):
+        if cpu > max_cpu or memory > max_mamory:
+            return False
+
+        self.allocations[task_id] = (cpu, memory)
+        return True
+
+    def get_allocation(self, task_id):
+        return self.allocations[task_id]
+
+    def is_allocated(self, task_id):
+        return self.allocations.has_key(task_id)
